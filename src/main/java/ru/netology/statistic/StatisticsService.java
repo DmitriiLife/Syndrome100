27ed21324ba998fc04d98841d5dfabbab9fda971
mac <dmitri-shadri@yandex.ru>
2021-09-06 08:21:45 +0300

    corrected all

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -1,6 +1,6 @@
 package ru.netology.statistic;
 
-public class StatisticsService{
+public class StatisticsService {
     /**
      * Calculate index of max income
      *
@@ -9,10 +9,52 @@ public class StatisticsService{
      */
     public long findMax(long[] incomes) {
         long current_max_index = 0;
-        long current_max = incomes[0];
+        long current_max = 0;
         for (long income : incomes)
-            if (current_max <= income)
+            if (current_max < income)
                 current_max = income;
         return current_max;
+    }
+    public long findSum(long[] incomes) {
+        long current_sum = 0;
+        for (long income : incomes)
+                current_sum += income;
+        return current_sum;
+    }
+
+    public long findMin(long[] incomes) {
+        int current_min_index = 0;
+        long current_min = incomes[0];
+        for (long income : incomes)
+            if (current_min > income)
+                current_min = income;
+        return current_min;
+    }
+
+    public long findAverage(long[] incomes) {
+        long sum = findSum(incomes);
+        return sum / incomes.length;
+    }
+
+    public long findAboveAverage(long[] incomes) {
+        double average = findAverage(incomes);
+        long Month = 0;
+        for (long income : incomes) {
+            if (income < average) {
+                Month++;
+            }
+        }
+        return Month;
+    }
+
+    public long findBelowAverage(long[] incomes) {
+        double average = findAverage(incomes);
+        long Month = 0;
+        for (long income : incomes) {
+            if (income > average) {
+                Month++;
+            }
+        }
+        return Month;
     }
 }
--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -9,12 +9,53 @@ class StatisticsServiceTest {
     @Test
     void findMax() {
         StatisticsService service = new StatisticsService();
-
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
-
         long actual = service.findMax(incomesInBillions);
+        assertEquals(expected, actual);
+    }
 
+    @Test
+    void findSum() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 85;
+        long actual = service.findSum(incomesInBillions);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMin() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long actual = service.findMin(incomesInBillions);
+        long expected = 3;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findAverage() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 7;
+        long actual = service.findAverage(incomesInBillions);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findAboveAverage() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long actual = service.findAboveAverage(incomesInBillions);
+        long expected = 5;
+        assertEquals(expected, actual);
+    }
+    @Test
+    void findBelowAverage() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long actual = service.findBelowAverage(incomesInBillions);
+        long expected = 6;
         assertEquals(expected, actual);
     }
 }
